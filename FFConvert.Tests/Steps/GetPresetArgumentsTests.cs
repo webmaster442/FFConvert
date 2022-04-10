48d3570d63fa7eb17652b9a9ba31a51abe228002
webmaster442 <ruzsinszki.gabor@gmail.com>
2022-04-10 18:35:30 +0200

    Get Preset arguments

---
+++ FFConvert.Tests/Steps/GetPresetArgumentsTests.cs
@@ -0,0 +1,204 @@
+﻿namespace FFConvert.Tests.Steps;
+
+[TestFixture]
+internal class GetPresetArgumentsTests : StepTestBase<GetPresetArguments>
+{
+    private Mock<IConsole> _consoleMock;
+
+    public override GetPresetArguments CreateSut()
+    {
+        _consoleMock = new Mock<IConsole>(MockBehavior.Strict);
+        _consoleMock.Setup(x => x.Write(It.IsAny<string>()));
+        _consoleMock.Setup(x => x.WriteLine(It.IsAny<string>()));
+        _consoleMock.Setup(x => x.Error(It.IsAny<string[]>()));
+
+        return new GetPresetArguments(new ImplementationsOf<IConverter>(),
+                                      new ImplementationsOf<IValidator>(),
+                                      _consoleMock.Object);
+
+    }
+
+    [Test]
+    public void EnsureThat_PresetHasNoParams_ReturnsTrue()
+    {
+        var state = CreateState("");
+        state.CurrentPreset = state.Presets[0];
+        var result = Sut.TryExecute(state);
+
+        Assert.IsTrue(result);
+        AssertHasNoIssues();
+    }
+
+
+    [Test, Timeout(1000)]
+    public void EnsureThat_SimpleParamInput_ReturnsTrue()
+    {
+        var state = CreateState("");
+        state.Presets[0].ParametersToAsk.Add(new PresetParameter
+        {
+            ParameterDescription = "test",
+            ParameterName = "testparam"
+        });
+        state.CurrentPreset = state.Presets[0];
+        _consoleMock.Setup(x => x.ReadLine()).Returns("foo");
+
+        var result = Sut.TryExecute(state);
+
+        _consoleMock.Verify(x => x.ReadLine(), Times.Once);
+
+        Assert.IsTrue(result);
+        AssertHasNoIssues();
+    }
+
+    [Test, Timeout(1000)]
+    public void EnsureThat_SimpleParamInput_WithConveter_ReturnsTrue()
+    {
+        var state = CreateState("");
+        state.Presets[0].ParametersToAsk.Add(new PresetParameter
+        {
+            ParameterDescription = "test",
+            ParameterName = "testparam",
+            ConverterName = nameof(KbpsConverter)
+        });
+        state.CurrentPreset = state.Presets[0];
+        _consoleMock.Setup(x => x.ReadLine()).Returns("foo");
+
+        var result = Sut.TryExecute(state);
+
+        _consoleMock.Verify(x => x.ReadLine(), Times.Once);
+        Assert.IsTrue(result);
+        AssertHasNoIssues();
+    }
+
+
+    [Test, Timeout(1000)]
+    public void EnsureThat_SimpleParamInput_OptionalParam_EmptyInput_ReturnsTrue()
+    {
+        var state = CreateState("");
+        state.Presets[0].ParametersToAsk.Add(new PresetParameter
+        {
+            ParameterDescription = "test",
+            ParameterName = "testparam",
+            IsOptional = true,
+        });
+        state.CurrentPreset = state.Presets[0];
+        _consoleMock.Setup(x => x.ReadLine()).Returns("");
+
+        var result = Sut.TryExecute(state);
+
+        _consoleMock.Verify(x => x.ReadLine(), Times.Once);
+        Assert.IsTrue(result);
+        AssertHasNoIssues();
+    }
+
+    [Test, Timeout(1000)]
+    public void EnsureThat_SimpleParamInput__WithIncorrectCorrectInput_AsksInputAgain()
+    {
+        var state = CreateState("");
+        state.Presets[0].ParametersToAsk.Add(new PresetParameter
+        {
+            ParameterDescription = "test",
+            ParameterName = "testparam"
+        });
+        state.CurrentPreset = state.Presets[0];
+        _consoleMock.SetupSequence(x => x.ReadLine()).Returns("").Returns("foo");
+
+        var result = Sut.TryExecute(state);
+
+        _consoleMock.Verify(x => x.ReadLine(), Times.Exactly(2));
+        Assert.IsTrue(result);
+        AssertHasNoIssues();
+    }
+
+    [Test, Timeout(1000)]
+    public void EnsureThat_BadPresetParams_ReturnsFalse()
+    {
+        var state = CreateState("");
+        state.Presets[0].ParametersToAsk.Add(new PresetParameter
+        {
+            ParameterDescription = "test",
+            ParameterName = "testparam",
+            ValidatorName = nameof(IntValidator),
+            ValidatorParameters = "min=2max=8"
+        });
+        state.CurrentPreset = state.Presets[0];
+
+
+        _consoleMock.Setup(x => x.ReadLine()).Returns("7");
+
+        var result = Sut.TryExecute(state);
+
+        Assert.IsFalse(result);
+        AssertHasIssues();
+    }
+
+
+    [Test, Timeout(1000)]
+    public void EnsureThat_ValidatedInput_WithCorrectInput_ReturnsTrue()
+    {
+        var state = CreateState("");
+        state.Presets[0].ParametersToAsk.Add(new PresetParameter
+        {
+            ParameterDescription = "test",
+            ParameterName = "testparam",
+            ValidatorName = nameof(IntValidator),
+            ValidatorParameters = "min=2;max=8"
+        });
+        state.CurrentPreset = state.Presets[0];
+
+
+        _consoleMock.Setup(x => x.ReadLine()).Returns("7");
+
+        var result = Sut.TryExecute(state);
+
+        _consoleMock.Verify(x => x.ReadLine(), Times.Once);
+        Assert.IsTrue(result);
+        AssertHasNoIssues();
+    }
+
+    [Test, Timeout(1000)]
+    public void EnsureThat_ValidatedInput_WithIncorrectCorrectInput_AsksInputAgain()
+    {
+        var state = CreateState("");
+        state.Presets[0].ParametersToAsk.Add(new PresetParameter
+        {
+            ParameterDescription = "test",
+            ParameterName = "testparam",
+            ValidatorName = nameof(IntValidator),
+            ValidatorParameters = "min=2;max=8"
+        });
+        state.CurrentPreset = state.Presets[0];
+
+
+        _consoleMock.SetupSequence(x => x.ReadLine()).Returns("").Returns("-1").Returns("8");
+        var result = Sut.TryExecute(state);
+
+        _consoleMock.Verify(x => x.Error(It.IsAny<string[]>()), Times.Once);
+        _consoleMock.Verify(x => x.ReadLine(), Times.Exactly(3));
+        Assert.IsTrue(result);
+        AssertHasNoIssues();
+    }
+
+    [Test, Timeout(1000)]
+    public void EnsureThat_ValidatedInput_OptionalParam_EmptyInput_ReturnsTrue()
+    {
+        var state = CreateState("");
+        state.Presets[0].ParametersToAsk.Add(new PresetParameter
+        {
+            ParameterDescription = "test",
+            ParameterName = "testparam",
+            ValidatorName = nameof(IntValidator),
+            ValidatorParameters = "min=2;max=8",
+            IsOptional = true
+        });
+        state.CurrentPreset = state.Presets[0];
+
+
+        _consoleMock.Setup(x => x.ReadLine()).Returns("");
+        var result = Sut.TryExecute(state);
+
+        _consoleMock.Verify(x => x.ReadLine(), Times.Exactly(1));
+        Assert.IsTrue(result);
+        AssertHasNoIssues();
+    }
+}
---
+++ FFConvert/Steps/GetPresetArguments.cs
@@ -0,0 +1,107 @@
+﻿using FFConvert.Domain;
+using FFConvert.DomainServices;
+using FFConvert.Interfaces;
+using FFConvert.Properties;
+
+namespace FFConvert.Steps;
+
+internal class GetPresetArguments : BaseStep
+{
+    private readonly IImplementationsOf<IConverter> _converters;
+    private readonly IImplementationsOf<IValidator> _validators;
+    private readonly IConsole _console;
+
+    public GetPresetArguments(IImplementationsOf<IConverter> converters,
+                              IImplementationsOf<IValidator> validators,
+                              IConsole console)
+    {
+        _converters = converters;
+        _validators = validators;
+        _console = console;
+    }
+
+    public override bool TryExecute(State state)
+    {
+        if (!state.CurrentPreset.ParametersToAsk.Any())
+            return true;
+
+        foreach (var parameter in state.CurrentPreset.ParametersToAsk)
+        {
+            string input;
+            if (!string.IsNullOrEmpty(parameter.ValidatorName))
+            {
+                input = ReadPresetValueWithValidator(parameter);
+            }
+            else
+            { 
+                input = ReadPresetValue(parameter); 
+            }
+            parameter.Value = ConvertValue(input, parameter);
+        }
+
+        return AreNoIssues();
+    }
+
+    private string ConvertValue(string input, PresetParameter parameter)
+    {
+        if (string.IsNullOrEmpty(parameter.ConverterName))
+            return input;
+
+        IConverter converter = _converters.Get(parameter.ConverterName);
+
+        return converter.Convert(input);
+    }
+
+    private string ReadPresetValueWithValidator(PresetParameter parameter)
+    {
+        IDictionary<string, string> paramDictionary = new Dictionary<string, string>();
+
+        if (parameter.ValidatorParameters != null
+            && !parameter.TryGetValidatorParamDictionary(out paramDictionary))
+        {
+            AddIssue(Resources.ErrorPresetParamTokens);
+            return string.Empty;
+        }
+
+        bool valid = false;
+        string input;
+        do
+        {
+            _console.Write(parameter.ParameterDescription);
+            _console.Write(" : ");
+            input = _console.ReadLine();
+
+            if (parameter.IsOptional == false && string.IsNullOrEmpty(input))
+            {
+                continue;
+            }
+
+            var validator = _validators.Get(parameter.ValidatorName!);
+
+            var (status, errorMessage) = validator.Validate(input, paramDictionary);
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                _console.Error(errorMessage);
+            }
+            valid = status;
+        }
+        while (!valid && !parameter.IsOptional);
+
+        return input;
+
+    }
+
+
+    private string ReadPresetValue(PresetParameter parameter)
+    {
+        string input;
+        do
+        {
+            _console.Write(parameter.ParameterDescription);
+            _console.Write(" : ");
+            input = _console.ReadLine();
+        }
+        while (string.IsNullOrEmpty(input) && !parameter.IsOptional);
+        return input;
+    }
+}

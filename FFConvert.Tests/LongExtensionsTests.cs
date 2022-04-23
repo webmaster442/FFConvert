b5e5130600252cdb55e5c38447d16a0ca36699e0
webmaster442 <ruzsinszki.gabor@gmail.com>
2022-04-23 11:41:36 +0200

    Long extensons tests

---
+++ FFConvert.Tests/LongExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿namespace FFConvert.Tests;
+
+internal class LongExtensionsTests
+{
+    [TestCase(128L, "128 bytes")]
+    [TestCase(1025L, "1.00 KiB")]
+    [TestCase(1048576L, "1.00 MiB")]
+    [TestCase(1074790400L, "1.00 GiB")]
+    [TestCase(1100585369600L, "1.00 TiB")]
+    public void EnsureThat_ToFileSize_ReturnsCorrect(long input, string expected)
+    {
+        string result = input.ToFileSize();
+        Assert.AreEqual(expected, result);
+    }
+}
--- FFConvert/DomainServices/LongExtensions.cs
+++ FFConvert/DomainServices/LongExtensions.cs
@@ -1,28 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 
-namespace FFConvert.DomainServices
+namespace FFConvert.DomainServices;
+
+internal static class LongExtensions
 {
-    internal static class LongExtensions
-    {
-        private const long KiB = 1024;
-        private const long MiB = KiB * 1024;
-        private const long GiB = MiB * 1024;
-        private const long TiB = GiB * 1024;
+    private const long KiB = 1024;
+    private const long MiB = KiB * 1024;
+    private const long GiB = MiB * 1024;
+    private const long TiB = GiB * 1024;
 
-        public static string ToFileSize(this long size)
+    public static string ToFileSize(this long size)
+    {
+        return size switch
         {
-            return size switch
-            {
-                > TiB => $"{size / (double)TiB:0.00} TiB",
-                > GiB => $"{size / (double)GiB:0.00} GiB",
-                > MiB => $"{size / (double)MiB:0.00} MiB",
-                > 1024 => $"{size / (double)KiB:0.00} KiB",
-                _ => $"{size} bytes",
-            };
-        }
+            >= TiB => $"{(size / (double)TiB).ToString("0.00", CultureInfo.InvariantCulture)} TiB",
+            >= GiB => $"{(size / (double)GiB).ToString("0.00", CultureInfo.InvariantCulture)} GiB",
+            >= MiB => $"{(size / (double)MiB).ToString("0.00", CultureInfo.InvariantCulture)} MiB",
+            >= 1024 => $"{(size / (double)KiB).ToString("0.00", CultureInfo.InvariantCulture)} KiB",
+            _ => $"{size} bytes",
+        };
     }
 }

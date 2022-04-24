e503bbc455bcee9315fa31b2cd74022371a5345b
webmaster442 <ruzsinszki.gabor@gmail.com>
2022-04-24 18:20:15 +0200

    FfMpeg install feature groundwork

---
+++ FFConvert/Infrastructure/FFMpegInstaller.cs
@@ -0,0 +1,35 @@
+﻿
+using FFConvert.Interfaces;
+
+namespace FFConvert.Infrastructure;
+
+internal class FFMpegInstaller
+{
+    private readonly IConsole _console;
+    private readonly string _updateFile;
+
+    public FFMpegInstaller(IConsole console, string file = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z")
+    {
+        _console = console;
+        _updateFile = file;
+    }
+
+    public async Task Install(CancellationToken token)
+    {
+        var targetFile = Path.Combine(Path.GetTempPath(), "ffmpeg.7z");
+        try
+        {
+            using var client = new HttpClient();
+            using var s = await client.GetStreamAsync(_updateFile, token);
+            using var fs = File.Create(targetFile);
+            await s.CopyToAsync(fs, token);
+
+
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+}
